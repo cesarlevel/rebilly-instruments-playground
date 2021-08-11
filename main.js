75a21778eae1218215b7d019cc06904ef61e28f0
Cesar Level <1580169+cesarlevel@users.noreply.github.com>
2021-08-11 16:39:47 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="sites/site-a.html">Go to site a</a>
+`
---
+++ sites/site-a.js
@@ -0,0 +1,6 @@
+import '../style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello A!</h1>
+  <a href="../index.html">Go to back to menu</a>
+`
