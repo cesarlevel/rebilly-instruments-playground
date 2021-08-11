7552b2ecdee1414a2f9553105c35423c869492fe
Cesar Level <1580169+cesarlevel@users.noreply.github.com>
2021-08-11 16:54:39 -0400

    Tweak main.html

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1>Rebilly Instruments Playground</h1>
   <a href="sites/site-a.html">Go to site a</a>
 `
--- sites/site-a.js
+++ sites/site-a.js
@@ -1,6 +1,6 @@
 import '../style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello A!</h1>
+  <h1>Site A!</h1>
   <a href="../index.html">Go to back to menu</a>
 `
